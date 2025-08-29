376b1febaaf8355263476a3cd481dd4957b60969
Kriti Tripathi <tripathikriti483@gmail.com>
2025-08-29 16:09:06 +0545

    prisma client coded

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+import { PrismaClient } from "../generated/prisma/index.js";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.userDetail.create({
+    data: {
+      username: "Kriti",
+      email: "kriti@example.com",
+      password: "securepassword123",
+      role: "customer",
+      gender: "female",
+    },
+  });
+
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
